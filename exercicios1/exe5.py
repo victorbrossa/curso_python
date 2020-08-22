16e052a7187ef5bd00455595e5a0d7dd815a554a
Victor Luiz <victorbrossa@hotmail.com>
2020-08-22 13:59:26 -0300

    finalizando o exericio5

--- exercicios1/exe5.py
+++ exercicios1/exe5.py
@@ -1,17 +1,17 @@
 '''Escreva um programa que receba dois números e um sinal, 
 e faça a operação matemática definida pelo sinal. '''
 
-num1 = int (input('Digite primeiro numero: '))
+nume1 = int (input('Digite primeiro numero: '))
 operador = input('Digite o operador: ')
-num2 = int (input('Digite segundo numero: '))
+nume2 = int (input('Digite segundo numero: '))
 
 if operador == '+':
-        resultado = num1 + num2
+        resultado = nume1 + nume2
 elif operador == '-':
-        resultado = num1 - num2
+        resultado = nume1 - nume2
 elif operador == '/':
-        resultado = num1 / num3
+        resultado = nume1 / nume2
 elif operador == '*':
-        resultado = num1 * num3
+        resultado = nume1 * nume2
 
 print (resultado)       
